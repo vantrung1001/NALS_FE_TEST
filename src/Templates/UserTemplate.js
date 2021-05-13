337118256651202701162b3c15071451bc3dac8f
Trung Ngo <vantrung1001@gmail.com>
2021-05-13 21:34:31 +0700

    register ui

--- src/Templates/UserTemplate.js
+++ src/Templates/UserTemplate.js
@@ -10,7 +10,7 @@ export default function UserTemplate(props) {
                 <div className="container-fluid page-body-wrapper full-page-wrapper">
                     <div className="content-wrapper d-flex align-items-stretch auth auth-img-bg">
                         <div className="row flex-grow align-items-center px-5">
-                            <div className="col-lg-4 py-4 mx-auto bg-white" style={{ height: 600 }}>
+                            <div className="col-xl-4 col-md-6 py-4 mx-auto bg-white" style={{ height: 'auto' }}>
                                 <Component {...propsRoute} />
                             </div>
                         </div>
